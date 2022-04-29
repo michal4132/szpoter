3f745ee0d45c072feb3e3cb213fa688bbcb404eb
Michał <michal.b2000@onet.pl>
2022-04-29 20:31:36 +0200

    move logger to namespace

--- include/Log.h
+++ include/Log.h
@@ -4,21 +4,25 @@
 #include <string>
 #include <stdarg.h>
 
-static constexpr const char *colorReset = "\e[0m";
-static constexpr const char *colorRed = "\e[0;31m";
-static constexpr const char *colorBlue = "\e[0;34m";
-static constexpr const int NColors = 15;
-static constexpr int allColors[NColors] = {30, 32, 33, 34, 35, 36, 37, 91, 92, 93, 94, 95, 96, 97, 98};
+namespace logger {
 
-void debug(std::string filename, int line, std::string submodule, const char *format, ...);
-void error(std::string filename, int line, std::string submodule, const char *format, ...);
-void info(std::string filename, int line, std::string submodule, const char *format, ...);
-void setSubmodule();
+    static constexpr const char *colorReset = "\e[0m";
+    static constexpr const char *colorRed = "\e[0;31m";
+    static constexpr const char *colorBlue = "\e[0;34m";
+    static constexpr const int NColors = 15;
+    static constexpr int allColors[NColors] = {30, 32, 33, 34, 35, 36, 37, 91, 92, 93, 94, 95, 96, 97, 98};
+
+    void debug(std::string filename, int line, std::string submodule, const char *format, ...);
+    void error(std::string filename, int line, std::string submodule, const char *format, ...);
+    void info(std::string filename, int line, std::string submodule, const char *format, ...);
+    void setSubmodule();
+
+}
 
 #define LOG(type, ...)                                      \
     do                                                      \
     {                                                       \
-        type(__FILE__, __LINE__, "szpoter", __VA_ARGS__);   \
+        logger::type(__FILE__, __LINE__, "szpoter", __VA_ARGS__);   \
     } while (0)
 
 #endif  // __LOG_H__
--- src/Log.cpp
+++ src/Log.cpp
@@ -1,70 +1,74 @@
 #include "Log.h"
 
-static bool enableSubmodule = false;
+namespace logger {
 
-void setSubmodule() {
-    enableSubmodule = true;
+    static bool enableSubmodule = false;
+
+    void setSubmodule() {
+        enableSubmodule = true;
+    }
+
+    void printFilename(std::string filename) {
+        std::string basenameStr(filename.substr(filename.rfind("/") + 1));
+        unsigned long hash = 5381;
+        for (char const &c : basenameStr) {
+            hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        }
+
+        printf("\e[0;%dm", allColors[hash % NColors]);
+
+        printf("%s", basenameStr.c_str());
+        printf(colorReset);
+    }
+
+    void debug(std::string filename, int line, std::string submodule, const char *format, ...) {
+        printf(colorRed);
+        printf("D ");
+        if (enableSubmodule) {
+            printf(colorReset);
+            printf("[%s] ", submodule.c_str());
+        }
+        printFilename(filename);
+        printf(":%d: ", line);
+        va_list args;
+        va_start(args, format);
+        vprintf(format, args);
+        va_end(args);
+        printf("\n");
+    };
+
+    void error(std::string filename, int line, std::string submodule, const char *format, ...) {
+        printf(colorRed);
+        printf("E ");
+        if (enableSubmodule) {
+            printf(colorReset);
+            printf("[%s] ", submodule.c_str());
+        }
+        printFilename(filename);
+        printf(":%d: ", line);
+        printf(colorRed);
+        va_list args;
+        va_start(args, format);
+        vprintf(format, args);
+        va_end(args);
+        printf("\n");
+    };
+
+    void info(std::string filename, int line, std::string submodule, const char *format, ...) {
+        printf(colorBlue);
+        printf("I ");
+        if (enableSubmodule) {
+            printf(colorReset);
+            printf("[%s] ", submodule.c_str());
+        }
+        printFilename(filename);
+        printf(":%d: ", line);
+        printf(colorReset);
+        va_list args;
+        va_start(args, format);
+        vprintf(format, args);
+        va_end(args);
+        printf("\n");
+    };
+
 }
-
-void printFilename(std::string filename) {
-    std::string basenameStr(filename.substr(filename.rfind("/") + 1));
-    unsigned long hash = 5381;
-    for (char const &c : basenameStr) {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-
-    printf("\e[0;%dm", allColors[hash % NColors]);
-
-    printf("%s", basenameStr.c_str());
-    printf(colorReset);
-}
-
-void debug(std::string filename, int line, std::string submodule, const char *format, ...) {
-    printf(colorRed);
-    printf("D ");
-    if (enableSubmodule) {
-        printf(colorReset);
-        printf("[%s] ", submodule.c_str());
-    }
-    printFilename(filename);
-    printf(":%d: ", line);
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
-    printf("\n");
-};
-
-void error(std::string filename, int line, std::string submodule, const char *format, ...) {
-    printf(colorRed);
-    printf("E ");
-    if (enableSubmodule) {
-        printf(colorReset);
-        printf("[%s] ", submodule.c_str());
-    }
-    printFilename(filename);
-    printf(":%d: ", line);
-    printf(colorRed);
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
-    printf("\n");
-};
-
-void info(std::string filename, int line, std::string submodule, const char *format, ...) {
-    printf(colorBlue);
-    printf("I ");
-    if (enableSubmodule) {
-        printf(colorReset);
-        printf("[%s] ", submodule.c_str());
-    }
-    printFilename(filename);
-    printf(":%d: ", line);
-    printf(colorReset);
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
-    printf("\n");
-};
